4038da8c9305207adc0c60cc0f51e883ff42f573
Cyril Gravelier <cyril@18ruedivona.eu>
2011-12-15 21:34:19 +0100

    Add local jquery

--- scraper.js
+++ scraper.js
@@ -38,7 +38,7 @@ module.exports = function() {
       // also tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
       jsdom.env({
         html: body,
-        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+        scripts: ['./jquery-1.6.min.js']
       }, function(err, window) {
         // Use jQuery just as in regular HTML page
         var $ = window.jQuery;
